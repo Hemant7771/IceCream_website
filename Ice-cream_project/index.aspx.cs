ca5307503617952fe0a142e83177e61228ff362e
Hemant7771 <hemantborn82@gmail.com>
2023-10-31 10:54:17 +0530

    first commit

---
+++ Ice-cream_project/index.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Ice_cream_project
+{
+	public partial class WebForm1 : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			/*if (!IsPostBack)
+            {
+                string connectionString = "Server=localhost;Database=product;";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                   // connection.Open();
+
+                    string query = "SELECT * FROM ice_cream";
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                        {
+                            DataTable dataTable = new DataTable();
+                            //adapter.Fill(dataTable);
+
+                            
+                        }
+                    }
+                }
+            }*/
+		}
+	}
+}
